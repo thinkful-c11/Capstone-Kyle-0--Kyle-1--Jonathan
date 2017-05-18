89d97eebd5e688e4fd39b3b251c8746ac3442c4c
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-18 11:59:35 -0400

    Make google map appear

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //Google Maps Javascript API
-//AIzaSyDuNzKBvsQPptdGctcDVnOB3JzondJbwHA
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
